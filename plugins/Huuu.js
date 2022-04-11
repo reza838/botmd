addb60bcdeee716a2d08771cae2456308c90e752
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-04-12 04:50:29 +0700

    Update Huuu.js

--- plugins/Huuu.js
+++ plugins/Huuu.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "sticker by", author: "Elyas" })
+conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "sticker by", author: "Reza" })
 }
 
 handler.customPrefix = /^(huuu)$/i
