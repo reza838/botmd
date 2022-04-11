48ec20e777818ae9c8348101f3cbdf75a41e6e8b
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-04-12 04:08:22 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6287892711054', 'Hyzerr', m)
+conn.sendContact(m.chat, '6288217843188', 'Reza Temayang', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
