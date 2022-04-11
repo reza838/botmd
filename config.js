953e32dda903b0db488c3745ef173f6461ee7916
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-04-11 11:15:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,11 +43,11 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
-global.author = `𝚂𝙷𝙸𝚁𝙾-𝙼𝙳`
-global.wm = '𝙷𝚈𝚉𝙴𝚁 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻'
+global.author = `Reza Temayang`
+global.wm = 'Reza Temayang'
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/hyzer-stfu'
+global.instagram = 'https://www.instagram.com/reteam.id/'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
